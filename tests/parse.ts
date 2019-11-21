dc2a28a754d426ab45290d6c06f3f783aa00d241
sergey <sergio_89@ukr.net>
2019-11-21 10:48:09 +0200

    class name analyzer added

--- tests/parse.ts
+++ tests/parse.ts
@@ -2,6 +2,7 @@ import { AstParser } from ".."
 import { extractMethods } from "../analyzers/extract_all_methods"
 import { extractMethodName } from "../analyzers/extract_method_name"
 import { extractMethodParams } from "../analyzers/extractMethodParams"
+import { extractClassName } from "../analyzers/extract_class_name"
 
 const parser = new AstParser("./tests/test-data/episode.ts")
 
@@ -9,8 +10,13 @@ const ast = parser.parse()
 
 const methods = extractMethods(ast)
 
+const  className = extractClassName(ast)
+console.log(className)
+
 methods.forEach(m  => {
+    
     const methodName = extractMethodName(m)
     const params = extractMethodParams(m)
+
     console.log(`${methodName}(${params.join(",")})`)
 })
