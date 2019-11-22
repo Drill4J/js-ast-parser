3c6ba7729c9f480205fe9aac4577c3459ecb399e
sergey <sergio_89@ukr.net>
2019-11-22 17:27:09 +0200

    fix data output format

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import program from 'commander';
 import { AstParser } from './parser';
 import recursive from "recursive-readdir";
 import { writeJson } from "fs-extra"
+import { DataExtractor } from './extractor';
 
 function writeFile(name, object){
     writeJson(name, object, {spaces:2}, err => {
@@ -18,17 +19,18 @@ program
 program.parse(process.argv);
 
 const parser = new AstParser()
+const extractor = new DataExtractor(); 
 
 if (!program.folder){
     console.error("source code path should be specified via -f parameter")
 }
 
-
 recursive(program.folder ,["*.css", "*.html", "*.js"], function (err, files) {
     let results = []
     files.forEach(file => {
-        const ast = parser.parse(file)      
-        results.push({filePath:file, tree: ast})
+        const ast = parser.parse(file)
+        const data = extractor.getClassMethods(ast)      
+        results.push({filePath:file, result: data})
     });
     console.log(results)
     writeFile("all_result.json", results)
