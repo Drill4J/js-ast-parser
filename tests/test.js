d4ebaed1c25eea33a75c5bfc3c94b55f645ab07b
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-11-22 19:03:25 +0200

    cleanup

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,6 @@
 const test = require('ava');
 
 test("test", t => {
-    const a = 'hello world'
-    console.log(a)
-
     const parser = new AstParser()
     const ast = parser.parse("./tests/test-data/episode.ts")
     
