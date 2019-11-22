2f27ad30850084f115d00346ff7436d6d94b5e75
sergey <sergio_89@ukr.net>
2019-11-22 16:22:13 +0200

    file walker added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 #!/usr/bin/env ts-node
 import program from 'commander';
 import { AstParser } from './parser';
+import recursive from "recursive-readdir";
+import { writeJson } from "fs-extra"
+
+function writeFile(name, object){
+    writeJson(name, object, {spaces:2}, err => {
+        if (err) return console.error(err)
+      
+        console.log('success!')
+      })
+}
 
 program
   .requiredOption('-f, --folder <path>', 'source code path should be specified');
@@ -9,7 +19,21 @@ program.parse(process.argv);
 
 const parser = new AstParser()
 
-if (program.folder){
-    const ast = parser.parse(program.folder)
-    console.log(ast)
+if (!program.folder){
+    console.error("source code path should be specified via -f parameter")
 }
+
+
+recursive(program.folder ,["*.css", "*.html", "*.js"], function (err, files) {
+    let results = []
+    files.forEach(file => {
+        const ast = parser.parse(file)      
+        results.push({filePath:file, tree: ast})
+    });
+    console.log(results)
+    writeFile("all_result.json", results)
+});
+
+
+
+
