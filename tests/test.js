8e84831bdc9d98cbddf9369c5f41a213477c92ee
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-11-22 19:01:42 +0200

    Added debug

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,10 @@
 const test = require('ava');
 
 test("test", t => {
-    
-    const parser = new AstParser()
+    const a = 'hello world'
+    console.log(a)
 
+    const parser = new AstParser()
     const ast = parser.parse("./tests/test-data/episode.ts")
     
 })
