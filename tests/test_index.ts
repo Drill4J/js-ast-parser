53ad800953c67553760ee4d6af5d4de846f5c70d
sergey <sergio_89@ukr.net>
2019-12-09 17:41:59 +0200

    tests

--- tests/test_index.ts
+++ tests/test_index.ts
@@ -1,15 +1,14 @@
 import test from 'ava';
 import { parseFiles } from 'src/utils';
+import { AstParser } from 'src/parser';
+import { DataExtractor } from 'src/extractor';
 
-test('fn() returns foo', t => {
-	// parseFiles("/home/sergey/Github/todomvc/examples/typescript-angular/js", "", [
-    //     "*.js.map",
-    //     "*.js",
-    //     "_all.ts"
-    // ],[
-    //     "libs",
-    //     "coverage",
-    //     "interfaces"
-    // ])
-    // t.is(files, [""])
+test('test can find all methods', t => {
+	const parser = new AstParser()
+    const extractor = new DataExtractor();
+
+    const ast = parser.parse("./fixtures/original/episode.ts")
+    const data = extractor.getClassMethods(ast)
+    
+    t.assert(data.methods.length === 16)
 });
