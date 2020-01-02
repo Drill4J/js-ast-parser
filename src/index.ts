7252b542a08a1c011e25f975f66cecffa74f407a
sergey <sergio_89@ukr.net>
2020-01-02 17:25:19 +0200

    add -g option for sample config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 #!/usr/bin/env node
 import program from 'commander';
-import { parseFiles } from './utils';
+import { parseFiles, writeFile } from './utils';
 import { readFileSync } from 'fs';
 
 program
-  .requiredOption('-c, --cfg <path>', 'path to config.json file')
+  .option('-c, --config <path>', 'path to config.json file')
+  .option('-g, --example-config <path>', 'generate sample config')
   .parse(process.argv);
 
-let config = JSON.parse(readFileSync(program.cfg).toString())
 
-parseFiles(config.projectName, config.source_dir, config.url, config.ignoreFiles, config.ignoreFolders)
+if(program.config){  
+  let config = JSON.parse(readFileSync(program.config).toString())
+
+  parseFiles(config.projectName, config.source_dir, config.url, config.ignoreFiles, config.ignoreFolders)
+} 
+
+if(program.exampleConfig){
+  const configSample = {
+    "projectName": "demo",
+    "source_dir": "./src",
+    "url": "http://localhost:8081/saveAst",
+    "ignoreFiles": [
+        "*.js.map",
+        "*.js",
+        "*.html",
+        "*.css",
+        "*.json"
+    ],
+    "ignoreFolders": [
+        "libs",
+        "coverage",
+        "interfaces"
+    ]
+  }
+
+  console.log(`Creating sample config file at ${program.exampleConfig}`)
+  writeFile(program.exampleConfig, configSample)
+}
 
 
 
