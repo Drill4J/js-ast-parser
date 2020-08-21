73473a6e44989fab03c33ca15c3b4ced45ec5b30
Roman Davlyatshin <davlyatshinra@gmail.com>
2020-08-21 19:00:08 +0400

    tests: moved helper to root of unit tests folder

--- __tests__/unit/extractors/parent-name-chain/spec.ts
+++ __tests__/unit/extractors/parent-name-chain/spec.ts
@@ -1,5 +1,5 @@
 import { AST_NODE_TYPES } from "@typescript-eslint/typescript-estree";
-import { prepareCtx } from '../../handlers/helper';
+import { prepareCtx } from '../../helper';
 import specimen from '../../../../src/extractors/parent-name-chain';
 
 describe('Parent name chain extractor', function () {
