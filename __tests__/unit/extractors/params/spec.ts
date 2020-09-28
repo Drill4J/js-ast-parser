dc47907895a04d13438b423efdc77540ed2eb9a6
Roman Davlyatshin <davlyatshinra@gmail.com>
2020-09-28 19:10:30 +0400

    chore: add license header + rule to check that

--- __tests__/unit/extractors/params/spec.ts
+++ __tests__/unit/extractors/params/spec.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { AST_NODE_TYPES } from '@typescript-eslint/typescript-estree';
 import { prepareCtx } from '../../helper';
 import specimen from '../../../../src/extractors/params';
